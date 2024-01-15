12a4e8eb2158ead1e9f97472b135ef43dcd72644
amiribnjaman <amiribnjaman@gmail.com>
2024-01-15 20:33:09 +0600

    Add recipe form created

--- src/app/componets/AddRecipeBtn.jsx
+++ src/app/componets/AddRecipeBtn.jsx
@@ -14,10 +14,18 @@ export default function AddRecipeBtn() {
       </button>
 
       {/*=====================*/}
-      <form className={`${showAddFrom ? "show" : "hidden"} absolute top-1/3 left-1/3 flex flex-col gap-2`} action="">
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Description" />
-        <input type="text" />
+      <form
+        className={`${
+          showAddFrom ? "show" : "hidden"
+        } absolute top-[130px] left-[450px] flex flex-col gap-2 w-[350px] min-h-[200px] bg-[#f2f6f3] px-3 py-4 rounded `}
+        action=""
+          >
+              <h3 className="text-center text-xl mb-2">Add a new Recipe</h3>
+        <input className="px-2 py-1.5" type="text" placeholder="Name" />
+        <input className="px-2 py-1.5" type="text" placeholder="Ingredients" />
+        <input className="px-2 py-1.5 bg-white" type="file" />
+              <textarea name="" id="" cols="10" rows="4"></textarea>
+              <input type="submit" value="Add Recipe" className="cursor-pointer bg-green-600 text-white mt-3 rounded py-1.5" />
       </form>
     </>
   );
